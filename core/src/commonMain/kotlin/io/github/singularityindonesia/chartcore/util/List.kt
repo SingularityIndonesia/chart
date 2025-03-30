a0e5620d435b2bcc0f436f21ea2f3507f56da34b
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-03-31 05:36:38 +0700

    doc: licencing

--- core/src/commonMain/kotlin/io/github/singularityindonesia/chartcore/util/List.kt
+++ core/src/commonMain/kotlin/io/github/singularityindonesia/chartcore/util/List.kt
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Chart.
+ * Copyright (C) 2025 singularity.indonesia@gmail.com
+ *
+ * Chart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Chart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package io.github.singularityindonesia.chartcore.util
 
 fun <T> List<T>.secondOrNull(): T? = runCatching { this[1] }.getOrNull()
